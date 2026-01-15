098652b2bb6547eeb4ab15360ad96036a54f823a
joelhmartin <joelhmartin@gmail.com>
2026-01-15 14:58:24 -0600

    3.3.96

--- anchor-tools.php
+++ anchor-tools.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Anchor Tools
  * Description: A set of tools provided by Anchor Corps. Lightweight Mega Menu, Popups, and bulk content editing using AI
- * Version: 3.3.95
+ * Version: 3.3.96
  * Author: Anchor Corps
  * Text Domain: anchor-tools
  */
